4dc776457ea373d891d037e3c0b0a5f4675746fc
Joshua Mangiola <joshm982@gmail.com>
2021-08-01 21:36:31 +1000

    Fixes Add Card Dialog

--- KanjiStudy.Web/Pages/AddCard.razor.cs
+++ KanjiStudy.Web/Pages/AddCard.razor.cs
@@ -1,15 +1,35 @@
+using KanjiStudy.SRS.Models;
 using KanjiStudy.Web.Data;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Threading.Tasks;
 
 namespace KanjiStudy.Web.Pages
 {
     public partial class AddCard
     {
+        [Inject]
+        public virtual LocalCardStore _localCardStore { get; set; }
         private CardModel cardModel = new();
         private string test;
-        private void HandleValidSubmit()
+        private async Task HandleValidSubmit()
         {
-            test = cardModel.EnglishMeaning;
-        // Process the valid form
+            var item1 = new RTKItem
+            {
+                Id = Guid.NewGuid().ToString(),
+                Number = cardModel.Number,
+                NumberStrokes = cardModel.NumberStrokes,
+                EnglishMeaning = cardModel.EnglishMeaning,
+                Kanji = cardModel.Kanji,
+                Story = cardModel.Story,
+                Notes = cardModel.Notes,
+                CorrectReviewStreak = 0,
+                DifficultyRating = 100,
+                PreviousCorrectReview = System.DateTime.MinValue,
+                ReviewDate = System.DateTime.MinValue
+            };
+
+            await _localCardStore.SaveVehicleAsync(item1);
         }
     }
 }
